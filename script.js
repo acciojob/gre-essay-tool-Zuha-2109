f02bdfe4224857b70c648e260d126b679f301bd9
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-04-03 12:20:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function myfun() {
+	let inps=document.getElementById("evaluatedText").value;
+	inps=inps.trim();
+
+	let s=inps.split(" ");
+	let size1=s.length;
+	/*if(inps[inps.length-1]=" "){
+		size1=size1-1;
+	}*/
+	if(inps.length==0){
+		document.getElementById("wordCount").innerHTML=0;
+	}else{
+		document.getElementById("wordCount").innerHTML=size1;
+	}
+	
+	
+} 
